a703a79ce3d51e18fdf4cc622bbcfe0763137e81
LUPUS23 <antoniodaw23@gmail.com>
2024-10-29 12:51:49 +0100

    Update

--- src/main/Duplicados.java
+++ src/main/Duplicados.java
@@ -1,6 +1,6 @@
 public class Duplicados {
     //Si entiendes el + 1, entiendes el ejercicio
-    public static String duplicats(int [] nums){
+    public static String duplicados(int [] nums){
         String resultado = "";
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
@@ -13,7 +13,7 @@ public class Duplicados {
     }
     public static void main(String[] args) {
         int [] nums = {12, 6, 2, 6, 5, 2, 12};
-        String resultado = duplicats(nums);
+        String resultado = duplicados(nums);
 
         System.out.println(resultado);
     }
