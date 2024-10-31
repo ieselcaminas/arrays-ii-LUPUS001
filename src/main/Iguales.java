743974a9c523b6c867edf1aa7a5175afb5897ea4
LUPUS23 <antoniodaw23@gmail.com>
2024-10-31 09:10:57 +0100

    Update

--- src/main/Iguales.java
+++ src/main/Iguales.java
@@ -1,19 +1,20 @@
 public class Iguales {
     public static boolean sonIguales(int [] array, int [] arrayClone){
-        boolean esIgual = false;
-        for (int i = 0; i < array.length; i++) {
-            for (int j = i + 1; j < array.length; j++) {
-                if (array[i] == arrayClone[j]){
-                    esIgual = true;
-                }
-            }
+        if (array.length != arrayClone.length){
+            return false;
         }
-        return esIgual;
+        for (int i = 0; i < array.length; i++) {
+                if (array[i] != arrayClone[i]){
+                    return false;
+                }
+        }
+        return true;
     }
     public static void main(String[] args) {
         int [] array = {12, 34, 2, 8};
         int [] arrayClone = array.clone();
 
         boolean esIgual = sonIguales(array, arrayClone);
+        System.out.println(esIgual);
     }
 }
